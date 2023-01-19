d54110d68642006414f8becc881b87a3b8b16fcc
YohaanS <107330605+YohaanS@users.noreply.github.com>
2023-01-18 17:48:20 -0700

    Add files via upload

---
+++ Html/Interact.js
@@ -0,0 +1,9 @@
+var clickerButton = document.getElementById("Fact-Button");
+var onButtonClick = function() {
+  clickerButton.textContent = "In the US, it costs roughly 2 cents to make a 1-cent coin. For a nickel (5 cents), it’s 7.5 cents."
+};
+
+clickerButton.addEventListener("click", onButtonClick);
+
+
+  
